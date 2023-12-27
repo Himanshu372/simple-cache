fa9eb07ba0805b1cf66ca48266a63488bc6b2221
Himanshu372 <jagtaphimanshu09@gmail.com>
2023-12-27 23:31:58 +0530

    feat: Commiting initial structure of cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Himanshu372/simple-cache
+
+go 1.21.4
---
+++ src/cache.go
@@ -0,0 +1,67 @@
+package src
+
+import (
+	"sort"
+)
+
+type LocalCache struct {
+	c    map[string]string
+	ru   map[string]int
+	size int
+}
+
+func NewLocalCache(size int) (*LocalCache, error) {
+	c := make(map[string]string, size)
+	ru := make(map[string]int, size)
+	return &LocalCache{
+		c:    c,
+		ru:   ru,
+		size: size,
+	}, nil
+}
+
+func (lc *LocalCache) Resize() {
+	if len(lc.c) == lc.size {
+		// Run eviction and evict top 10 items
+		lc.evict()
+	}
+	return
+}
+
+func (lc *LocalCache) Set(key string, val string) {
+	lc.Resize()
+	// Run eviction and store latest get key
+	lc.c[key] = val
+	return
+}
+
+func (lc *LocalCache) Get(key string) string {
+	if val, ok := lc.c[key]; ok {
+		lc.ru[key] += 1
+		return val
+	}
+	return ""
+}
+
+func (lc *LocalCache) Delete(key string) {
+	delete(lc.c, key)
+	delete(lc.ru, key)
+}
+
+type KeyUsedCount struct {
+	key string
+	ruc int
+}
+
+func (lc *LocalCache) evict() {
+	// Eviction is based on least recently used
+	kuc := make([]*KeyUsedCount, 0)
+	for k, count := range lc.ru {
+		kuc = append(kuc, &KeyUsedCount{k, count})
+	}
+	sort.Slice(kuc, func(i, j int) bool {
+		return kuc[i].ruc < kuc[j].ruc
+	})
+	lc.Delete(kuc[0].key)
+	return
+}
